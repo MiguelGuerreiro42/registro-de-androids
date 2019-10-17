575e1e107c3d6f8b79db1a90c8fab41406a44512
MiguelGuerreiro42 <miguelpachiega@hotmail.com>
2019-10-17 11:10:26 -0300

    Updates

---
+++ precache-manifest.b7ce325c905e5b61dc09408afb533a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e62c2a8c8f65fe115a40c0fd1d223b4",
+    "url": "/registro-de-androids/index.html"
+  },
+  {
+    "revision": "62d7d42853a5c8b7c0b6",
+    "url": "/registro-de-androids/static/css/main.dc861831.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/registro-de-androids/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "62d7d42853a5c8b7c0b6",
+    "url": "/registro-de-androids/static/js/main.97283e29.chunk.js"
+  },
+  {
+    "revision": "0de6bc39aac68d5a95f5",
+    "url": "/registro-de-androids/static/js/runtime-main.a43993bd.js"
+  }
+]);
